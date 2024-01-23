047dfb0d97e0bd54d4f7f5b0b814cfae45eda999
“Eltion <ebehrami01@gmail.com>
2024-01-23 14:24:26 -0500

    add changes

--- src/App.tsx
+++ src/App.tsx
@@ -49,6 +49,7 @@ export const App: FC = () => {
                         <PageHeader label="Featured Projects" />
                         <FeaturedProjects />
 
+
                         <PageHeader id="page-other-projects" label="Other Projects" />
                         <OtherProjects />
                     </Box>
